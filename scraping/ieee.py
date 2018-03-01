f926674999877be7441459fc2a168aa68475ec7a
Camille Roger <15185241+CamilleRoger@users.noreply.github.com>
2018-03-02 00:07:19 +0100

    update ieee.py (v2)
    Ajout des éléments manquants (notamment auteurs et mots-clés)
    Optimisation de la trame de l'algo
    Amélioration du paramétrage du fichier de sortie

--- scraping/ieee.py
+++ scraping/ieee.py
@@ -1,7 +1,3 @@
-""" Commande
-scrapy runspider ieee.py
-"""
-
 import re
 import csv
 import scrapy
@@ -10,8 +6,11 @@ from scrapy_splash import SplashRequest
 
 class IeeeSpider(scrapy.Spider):
     name = 'ieee'
-    # paramètrage pour faire le lien avec le serveur Splash local
+    fichier_json = 'ieee.json'
+    # purger le fichier de sortie JSON
+    open(fichier_json, 'w').close()
     custom_settings = {
+        # paramètrage pour faire le lien avec le serveur Splash local
         'SPLASH_URL': 'http://localhost:8050',
         'DOWNLOADER_MIDDLEWARES': {
             'scrapy_splash.SplashCookiesMiddleware': 723,
@@ -22,8 +21,10 @@ class IeeeSpider(scrapy.Spider):
             'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
         },
         'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
-        # 'DOWNLOAD_DELAY': 0.25,
-        # 'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.7',
+        'LOG_LEVEL': 'INFO',
+        'FEED_FORMAT': 'json',
+        'FEED_URI': fichier_json,
+        'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
     def start_requests(self):
@@ -31,34 +32,68 @@ class IeeeSpider(scrapy.Spider):
             lecteur = csv.reader(fichier_csv)
             next(lecteur)
             for numero_article in lecteur:
-                print(numero_article[0])
+                item = {}
+                item['id'] = numero_article[0]
                 yield SplashRequest(
                     url='http://ieeexplore.ieee.org/document/' + numero_article[0] + '/',
                     callback=self.parse,
-                    meta={'splash': {'args': {'wait': 2}}})
+                    meta={'splash': {'args': {'wait': 2}},
+                          'item': item})
 
     def parse(self, response):
-        rurl = response.url
-        print('url: ', rurl)
+        item = response.meta['item']
+        item['url'] = response.url
+        item['titre'] = response.css('h1.document-title > span.ng-binding::text').extract_first()
+        try:
+            item['date'] = re.compile(r'[\n\r\t]').sub('', response.css('div.u-pb-1.doc-abstract-confdate.ng-binding.ng-scope::text').extract()[2])
+            item['type'] = "conference"
+            item['lieu-conference'] = re.compile(r'[\n\r\t]').sub('', response.css('div.u-pb-1.doc-abstract-conferenceLoc.ng-binding.ng-scope::text').extract()[2])
+        except:
+            try:
+                item['date'] = re.compile(r'[\n\r\t]').sub('', response.css('div.u-pb-1.doc-abstract-pubdate.ng-binding.ng-scope::text').extract()[2])
+                item['type'] = "publication"
+            except:
+                item['date'] = None
+                item['type'] = None
 
-        titre = response.css('h1.document-title > span.ng-binding::text').extract_first()
-        print('titre: ', titre)
+        for compteur in response.css("button.document-banner-metric.ng-scope"):
+            donnees = compteur.css("div.ng-binding::text").extract()
+            if donnees[1] == "Citations":
+                item['nb-citations'] = int(donnees[0])
+            elif donnees[1] == "Text Views":
+                item['nb-vues'] = int(donnees[0])
 
-        auteurs = response.css('div.ng-scope.doc-ft-extra-padded > section.ng-isolate-scope > div.ng-scope > section.document-all-authors.ng-scope > div.ng-scope > div > div.author-container.stats-author-container.ng-scope > div.pure-u-18-24 > div > a > span.ng-binding::text').extract()
-        print('auteurs: ', auteurs)
+        item['resume'] = response.css('div.abstract-text.ng-binding::text').extract_first()
+        yield SplashRequest(url=response.url + 'keywords',
+                            callback=self.parse_mots_cles,
+                            meta={'splash': {'args': {'wait': 2}},
+                                  'item': item})
 
-        date = response.css('div.u-pb-1.doc-abstract-confdate.ng-binding.ng-scope::text').extract()[2]
-        print('date: ', date)
+    def parse_mots_cles(self, response):
+        item = response.meta['item']
+        for liste in response.css('li.doc-keywords-list-item.ng-scope'):
+            if liste.css('li.doc-keywords-list-item.ng-scope > strong.ng-binding::text').extract_first() == "IEEE Keywords":
+                item['mots-cles'] = liste.css('a.stats-keywords-list-item.ng-binding::text').extract()
+                break
+        yield SplashRequest(url=re.sub("keywords", "authors", response.url),
+                            callback=self.parse_auteurs,
+                            meta={'splash': {'args': {'wait': 2}},
+                                  'item': item})
 
-        nb_vues = response.css('div.document-banner-metric-count.ng-binding::text').extract()[1]
-        print('nb-vues: ', nb_vues)
-
-        resume = response.css('div.abstract-text.ng-binding::text').extract_first()
-        print('resume: ', resume)
-
-        # yield SplashRequest(url='http://ieeexplore.ieee.org/document/7495234/keywords', callback=self.parse_mots_cles)
-        # yield SplashRequest(url='http://ieeexplore.ieee.org/document/7495234/authors', callback=self.parse_auteurs)
-
-        # mots_cles = response.css('li.doc-keywords-list-item.ng-scope > ul.u-mt-1.u-p-0.List--no-style.List--inline > li.ng-binding.ng-scope > a.stats-keywords-list-item.ng-binding::text').extract()
-
-        # auteurs = response.css('div.pure-u-18-24 > div > a > span.ng-binding::text').extract()
+    def parse_auteurs(self, response):
+        item = response.meta['item']
+        noms_auteurs = response.css('div.pure-u-18-24 > div > a > span.ng-binding::text').extract()
+        infos_auteurs = response.css('div.pure-u-18-24 > div.ng-binding::text').extract()
+        nombre_auteurs = len(noms_auteurs)
+        auteurs = []
+        for i in range(nombre_auteurs):
+            auteur = {}
+            auteur['nom-auteur'] = noms_auteurs[i]
+            try:
+                auteur['infos-auteur'] = infos_auteurs[i]
+            except:
+                auteur['infos-auteur'] = None
+            auteurs.append(auteur)
+        item['auteurs'] = auteurs
+        print("Document n°", item['id'], "récolté.")
+        yield item
