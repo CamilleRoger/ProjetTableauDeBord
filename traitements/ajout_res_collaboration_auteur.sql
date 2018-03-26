79d8992cade6d1d7cbfc2b209c3c7bf5e4631ec4
Unknown <pierrick.herve56@gmail.com>
2018-03-26 12:11:44 +0200

    ajout req collaboration auteur

---
+++ traitements/ajout_res_collaboration_auteur.sql
@@ -0,0 +1,12 @@
+    curseur.execute("""SELECT aut1.nom_auteur, aut2.nom_auteur, COUNT(id_article) AS occurences
+					FROM Auteurs AS aut1
+					INNER JOIN Ecrire AS ecr1
+					ON aut1.id_auteur = ecr1.id_auteur
+					WHERE ecr.id_article = (SELECT ecr2.id_article
+											FROM Auteurs AS aut2
+											INNER JOIN Ecrire AS ecr2
+											ON aut2.id_auteur = ecr2.id_auteur)
+					GROUP BY aut1.nom_auteur, aut2.nom_auteur
+					ORDER By occurences;""")
+    for ligne in curseur:
+        print(ligne)
