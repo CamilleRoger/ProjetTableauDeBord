24840111210e418f27259690db2c7d354654e0d7
Camille Roger <15185241+CamilleRoger@users.noreply.github.com>
2018-03-26 11:13:00 +0200

    ajout nuage de mots

---
+++ traitements/nuage_de_mots.py
@@ -0,0 +1,25 @@
+from wordcloud import WordCloud
+
+
+# Read the whole text.
+text = open('csv/Top_des_mots_cles_de_rang_1.csv').read()
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+
+# lower max_font_size
+wordcloud = WordCloud(max_font_size=40).generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
+
+# The pil way (if you don't have matplotlib)
+# image = wordcloud.to_image()
+# image.show()
